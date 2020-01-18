d5902bbea50b6f4a1a6cb3f19b0b7cab4ef7dffe
ButchStein <44380390+ButchStein@users.noreply.github.com>
2020-01-18 17:03:21 +0300

    Delete keys.dev.js

--- server/keys/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGO_URI:`mongodb://vadim:V159753@first-shard-00-00-bguaa.mongodb.net:27017,first-shard-00-01-bguaa.mongodb.net:27017,first-shard-00-02-bguaa.mongodb.net:27017/nuxt-blog?ssl=true&replicaSet=first-shard-0&authSource=admin&retryWrites=true&w=majority`,
-  JWT: 'dev-jwt-key'
-}
